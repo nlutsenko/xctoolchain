e09623ee0875a0fa6f54a47a6b0bbbb0e0ca0a76
Nathan Kellert <nathankellert@gmail.com>
2020-01-03 12:29:53 -0600

    update OS version and simulator name

--- Scripts/xctask/build_framework_task.rb
+++ Scripts/xctask/build_framework_task.rb
@@ -77,7 +77,7 @@ module XCTask
     def build_ios_framework
       framework_paths = []
       framework_paths << build_framework('iphoneos')
-      framework_paths << build_framework('iphonesimulator', '"platform=iOS Simulator,name=iPhone 6s"')
+      framework_paths << build_framework('iphonesimulator', '"platform=iOS Simulator,name=iPhone 11,OS=13.2"')
       final_path = final_framework_path
 
       system("rm -rf #{final_path} && cp -R #{framework_paths[0]} #{final_path}")
@@ -127,7 +127,7 @@ module XCTask
     def build_tvos_framework
       framework_paths = []
       framework_paths << build_framework('appletvos')
-      framework_paths << build_framework('appletvsimulator', '"platform=tvOS Simulator,OS=12.2,name=Apple TV 4K"')
+      framework_paths << build_framework('appletvsimulator', '"platform=tvOS Simulator,OS=13.2,name=Apple TV 4K"')
       final_path = final_framework_path
 
       system("rm -rf #{final_path} && cp -R #{framework_paths[0]} #{final_path}")
